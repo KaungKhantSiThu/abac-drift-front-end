6ce5e4cb94dc5d6660894f586a9c01ff221cefc6
KaungKhantSiThu <64033794+KaungKhantSiThu@users.noreply.github.com>
2023-03-12 00:19:44 +0700

    HomePage update
    Implemented dynamic ProductList view populated by mock data

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,131 @@
 <script setup>
+import NavBar from "./components/NavBar.vue";
+import ProductList from "./components/ProductList.vue";
 
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Sample Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+  <header>
+    <nav-bar />
+  </header>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="mb-3">
+          <div>
+            <h5 class="text-start"><i class="fa-solid fa-location-dot"></i> Current Location</h5>
+            <input class="form-control" type="text" placeholder="Bangkok, 10240"
+                   aria-label="default input example">
+          </div>
+        </div>
+        <div class="card">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item h5 text-center">Filter By</li>
+            <li class="list-group-item">
+              <h6>Brand</h6>
+              <select class="form-select" id="autoSizingSelect">
+                <option selected>Choose brand</option>
+                <option value="1">Toyota</option>
+                <option value="2">Mercedes</option>
+                <option value="3">Honda</option>
+                <option value="4">BMW</option>
+                <option value="3">Ford</option>
+                <option value="4">Mazda</option>
+              </select>
+            </li>
+            <li class="list-group-item">
+              <h6>Model</h6>
+              <select class="form-select" id="autoSizingSelect" disabled>
+                <option selected>Choose model</option>
+                <option value="1">H247</option>
+                <option value="2">W223</option>
+                <option value="3">W206</option>
+                <option value="4">H243</option>
+                <option value="3">R50 One</option>
+                <option value="4">X243</option>
+              </select>
+            </li>
+            <li class="list-group-item">
+              <h6>Mileage</h6>
+              <!-- <span><i class="fa-solid fa-chevron-down"></i></span> -->
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                  0 - 5,000 km
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                  5,000 - 10,000 km
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                  10,000 - 20,000 km
+                </label>
+              </div>
+
+            </li>
+            <li class="list-group-item">
+              <h6>Year</h6>
+              <select class="form-select" id="autoSizingSelect">
+                <option selected>Choose year</option>
+                <option value="1">2022</option>
+                <option value="2">2021</option>
+                <option value="3">2020</option>
+                <option value="4">2019</option>
+                <option value="3">2018</option>
+                <option value="4">2017</option>
+              </select>
+            </li>
+            <li class="list-group-item">
+              <h6>Price</h6>
+              <!-- <span><i class="fa-solid fa-chevron-down"></i></span> -->
+              <div class="row">
+                <div class="col">
+                  <select class="form-select" id="autoSizingSelect">
+                    <option selected>min</option>
+                    <option value="1">25,000 Baht</option>
+                    <option value="2">30,000 Baht</option>
+                    <option value="3">40,000 Baht</option>
+                    <option value="4">50,000 Baht</option>
+                    <option value="2">60,000 Baht</option>
+                    <option value="3">70,000 Baht</option>
+                  </select>
+                </div>
+                <div class="col">
+                  <select class="form-select" id="autoSizingSelect">
+                    <option selected>max</option>
+                    <option value="1">50,000 Baht</option>
+                    <option value="2">60,000 Baht</option>
+                    <option value="3">70,000 Baht</option>
+                    <option value="4">80,000 Baht</option>
+                    <option value="3">90,000 Baht</option>
+                    <option value="4">100,000 Baht</option>
+                  </select>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="d-grid gap-2 mt-3">
+          <button type="button" class="btn btn-primary">Search</button>
+        </div>
+      </div>
+      <div class="col-9">
+        <div class="row">
+          <h2>Recommended Cars</h2>
+        </div>
+        <div>
+          <div class="row my-3">
+            <product-list />
+          </div>
+        </div>
       </div>
     </div>
-  </nav>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card product-card">
+    <img :src="car.imageURL" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{ car.title }}</h5>
+      <h6 class="text-secondary">{{ car.manufacturer }}</h6>
+      <div class="row">
+        <div class="col text-center">
+          <h6 class="mb-2">Year</h6>
+          <p>{{ car.year }}</p>
+        </div>
+        <div class="col text-center">
+          <h6 class="mb-2">Engine</h6>
+          <p>{{ car.engine }}</p>
+        </div>
+        <div class="col text-center">
+          <h6 class="mb-2">Mileage</h6>
+          <p>{{ car.mileage }}</p>
+        </div>
+        <div class="col text-center">
+          <h6 class="mb-2">Gear</h6>
+          <p>{{ car.gear }}</p>
+        </div>
+      </div>
+      <div class="row mx-3">
+        <div class="col text-start">
+          <i class="fa-solid fa-eye"></i>
+          <span>500</span>
+        </div>
+        <div class="col text-end">
+          <h4><span>฿</span> {{ car.price }}</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  car: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.product-card {
+  width: 340px;
+	height: auto;
+	min-height: 450px;
+  padding: 10px 15px;
+	margin-bottom: 24px;
+	margin-right: 10px;
+	overflow: hidden;
+	transition: all 0.2s linear;
+	cursor: pointer;
+}
+</style>
